658e83186cba3d306466e72985f220869e7a4bad
IamZakwithak <itszak90210@aim.com>
2026-09-20 20:10:08 -0700

    fixed buried_item_entity
    removed logic and just made it always sparkle

--- gbjam-14/Scripts/buried_item_entity.gd
+++ gbjam-14/Scripts/buried_item_entity.gd
@@ -1,12 +1,6 @@
 class_name BuriedItemEntity extends Entity
 
-@onready var previous_state: bool = false
 @onready var animation_player: AnimationPlayer = $Sprite2D/AnimationPlayer
 
-func _process(_delta: float) -> void:
-	if self.diggable != previous_state:
-		previous_state = self.diggable
-		if previous_state:
-			animation_player.play("Item_Buried")
-		else:
-			animation_player.play("RESET")
+func _ready():
+	animation_player.play("Item_Buried")
